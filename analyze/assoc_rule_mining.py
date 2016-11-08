eb993508c13f5fa22c515f8c79614917b21ab81b
Amit Arora <aa1603@georgetown.edu>
2016-11-07 20:36:04 -0500

    Re-run with new support levels for association rule mining.

--- analyze/assoc_rule_mining.py
+++ analyze/assoc_rule_mining.py
@@ -10,7 +10,7 @@ import os
 from common import globals as glob
 
 MIN_CONFIDENCE = 0.25
-MIN_SUPPORT_LIST = [0.05, 0.1, 0.15, 0.5]
+MIN_SUPPORT_LIST = [0.05, 0.07, 0.1]
 
 def mine_rule_2_features(df, feature1, feature2, feature1_shortname, feature2_shortname, assoc_rules):
     #find the frequency of all possible pairs of values of these features
